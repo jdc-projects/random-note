77c537398a4bd1a90563cba7a2a9e5bfe8e524d1
Jack Chapman <jack.chapman@sky.com>
2026-04-13 17:51:22 +0100

    Some test setup fixes.

--- tests/setup.ts
+++ tests/setup.ts
@@ -19,3 +19,38 @@ global.ResizeObserver = class ResizeObserver {
   unobserve() {}
   disconnect() {}
 };
+
+const noop = () => {};
+const fakeCanvas2dContext = {
+  measureText: () => ({ width: 0 }),
+  fillText: noop,
+  strokeText: noop,
+  fillRect: noop,
+  strokeRect: noop,
+  clearRect: noop,
+  save: noop,
+  restore: noop,
+  beginPath: noop,
+  closePath: noop,
+  moveTo: noop,
+  lineTo: noop,
+  rect: noop,
+  fill: noop,
+  stroke: noop,
+  arc: noop,
+  scale: noop,
+  rotate: noop,
+  translate: noop,
+  setFont: noop,
+  canvas: { width: 500, height: 200 },
+};
+
+HTMLCanvasElement.prototype.getContext = function (
+  contextId: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+): any {
+  if (contextId === "2d") {
+    return fakeCanvas2dContext;
+  }
+  return null;
+};
